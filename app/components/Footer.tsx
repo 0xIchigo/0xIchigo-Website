78a94b19aa0ccb3b644430c86409ed17fec8b231
Evan <0xIchigo@protonmail.com>
2023-05-22 15:16:55 -0400

    Update Footer to Include Email

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FaTwitter, FaGithub } from "react-icons/fa";
+import { GrMail } from "react-icons/gr";
 
 export default function Footer() {
     return (
@@ -7,6 +8,9 @@ export default function Footer() {
             <div className="flex items-center justify-center mx-auto p-4 text-2xl">
                 <Link href="https://twitter.com/0xIchigo" className="text-white mx-2">
                     <FaTwitter />
+                </Link>
+                <Link href="mailto:0xIchigo@protonmail.com" className="text-white mx-2">
+                    <GrMail />
                 </Link>
                 <Link href="https://github.com/0xIchigo" className="text-white mx-2">
                     <FaGithub />
