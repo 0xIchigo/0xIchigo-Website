140829d9bedb3550f8ef536bd4f288e21991775e
Evan <0xIchigo@protonmail.com>
2023-05-24 15:12:09 -0400

    Fix Build Errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
+/*
+  This is so annoying but the only work around until Next's app directory is more finely tuned
+  It seems that the app dir is using a compiler which doesn't like wrapped `require` in UMD builds
+  I'll have to find a way to tinker with the compiler or wait for Next to further update it
+  For now, this annoying "use client" statement and lazy loading Background will have to do
+*/
+"use client";
+
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Background from "./components/Background";
+//import Background from "./components/Background";
 import ProfilePic from "./components/ProfilePic";
 import Footer from "./components/Footer";
+
+import dynamic from "next/dynamic";
+const Background = dynamic(() => import("./components/Background"), { ssr: false });
 
 
 export const metadata = {
