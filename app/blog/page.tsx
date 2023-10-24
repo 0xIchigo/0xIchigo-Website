88cad2fb62642c469ab9263b8cd7e95ddfc96b0b
Evan <0xIchigo@protonmail.com>
2023-10-24 02:02:17 -0400

    Add Solana Enterprise Article

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -117,6 +117,18 @@ export default function Blog() {
                             Published October 14, 2023
                         </p>
                     </p>
+                    <p className="prose text-black min-w-full">
+                        <a 
+                            href="https://www.helius.dev/blog/evaluating-solana-for-enterprise-use-a-comprehensive-guide" 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Evaluating Solana for Enterprise Use: A Comprehensive Guide
+                        </a>
+                        <p className="prose text-sm ml-4">
+                            Published October 20, 2023
+                        </p>
+                    </p>
                 </div>
             </div>
         </main>
