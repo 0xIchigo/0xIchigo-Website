306b7793b44adade380087f772eea8691417b7c5
Evan <0xIchigo@protonmail.com>
2024-03-10 14:46:58 -0400

    Refactor Other Articles

---
+++ lib/blogPosts.tsx
@@ -0,0 +1,100 @@
+export interface BlogPost {
+    name: string;
+    link: string;
+    date: string;
+}
+
+const blogPosts: BlogPost[] = [
+    {
+        name: "Privacy on Solana with Elusiv and Light: All You Need to Know About Privacy on SOL",
+        link: "https://www.helius.dev/blog/privacy-on-solana-with-elusiv-and-light",
+        date: "September 13, 2023",
+    },
+    {
+        name: "Updated the Confidential Transfers Section for the What is Token-2022? Article",
+        link: "https://www.helius.dev/blog/what-is-token-2022",
+        date: "September 14, 2023",
+    },
+    {
+        name: "How to Mitigate Spam QUICkly: All You Need to Know About Solana and QUIC",
+        link: "https://www.helius.dev/blog/all-you-need-to-know-about-solana-and-quic",
+        date: "September 18, 2023",
+    },
+    {
+        name: "Proof of History, Proof of Stake, Proof of Work - Explained",
+        link: "https://www.helius.dev/blog/proof-of-history-proof-of-stake-proof-of-work-explained",
+        date: "September 21, 2023",
+    },
+    {
+        name: "Cryptographic Tools 101 - Hash Functions and Merkle Trees Explained",
+        link: "https://www.helius.dev/blog/cryptographic-tools-101-hash-functions-and-merkle-trees-explained",
+        date: "September 25, 2023",
+    },
+    {
+        name: "All You Need to Know About Compression on Solana",
+        link: "https://www.helius.dev/blog/all-you-need-to-know-about-compression-on-solana",
+        date: "October 3, 2023",
+    },
+    {
+        name: "All You Need to Know About Solana's 1.16 Update",
+        link: "https://www.helius.dev/blog/all-you-need-to-know-about-solanas-v1-16-update",
+        date: "October 6, 2023",
+    },
+    {
+        name: "Solana Geyser Plugins: Streaming Data at the Speed of Light",
+        link: "https://www.helius.dev/blog/solana-geyser-plugins-streaming-data-at-the-speed-of-light",
+        date: "October 14, 2023",
+    },
+    {
+        name: "Evaluating Solana for Enterprise Use: A Comprehensive Guide",
+        link: "https://www.helius.dev/blog/evaluating-solana-for-enterprise-use-a-comprehensive-guide",
+        date: "October 20, 2023",
+    },
+    {
+        name: "The Solana Programming Model: An Introduction to Developing on Solana",
+        link: "https://www.helius.dev/blog/the-solana-programming-model-an-introduction-to-developing-on-solana",
+        date: "November 10, 2023",
+    },
+    {
+        name: "What is Firedancer? A Deep Dive into Solana 2.0",
+        link: "https://www.helius.dev/blog/what-is-firedancer",
+        date: "November 29, 2023",
+    },
+    {
+        name: "Gaming: How Solana is Changing the Playfield",
+        link: "https://www.helius.dev/blog/gaming-how-solana-is-changing-the-playfield",
+        date: "December 11, 2023",
+    },
+    {
+        name: "Priority Fees: Understanding Solana's Transaction Fee Mechanics",
+        link: "https://www.helius.dev/blog/priority-fees-understanding-solanas-transaction-fee-mechanics",
+        date: "December 13, 2023",
+    },
+    {
+        name: "How to Migrate From Ethereum to Solana: A Guide for Devs",
+        link: "https://www.helius.dev/blog/how-to-migrate-from-ethereum-to-solana",
+        date: "December 28, 2023",
+    },
+    {
+        name: "An Introduction to Anchor: A Beginner's Guide to Building Solana Programs",
+        link: "https://www.helius.dev/blog/an-introduction-to-anchor-a-beginners-guide-to-building-solana-programs",
+        date: "January 31, 2024",
+    },
+    {
+        name: "All You Need to Know About Solana's v1.17 Update",
+        link: "https://www.helius.dev/blog/all-you-need-to-know-about-solanas-v1-17-update",
+        date: "February 13, 2024",
+    },
+    {
+        name: "Solana Frames: Minting a cNFT on Farcaster",
+        link: "https://www.helius.dev/blog/solana-frames-minting-a-cnft-on-farcaster",
+        date: "February 28, 2024",
+    },
+    {
+        name: "Futarchy and Governance: Prediction Markets Meet DAOs on Solana",
+        link: "March 8, 2024",
+        date: "https://www.helius.dev/blog/futarchy-and-governance-prediction-markets-meet-daos-on-solana",
+    },
+];
+
+export default blogPosts;
